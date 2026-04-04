a68a6bbb9191d7666e403cdd8df65791e46844be
woobeek <vasily.sorokin02@gmail.com>
2026-04-04 23:27:51 +0500

    Fix: build from frontend, add use client to hooks, fix CSS syntax

--- frontend/src/hooks/useSlots.js
+++ frontend/src/hooks/useSlots.js
@@ -1,3 +1,4 @@
+'use client';
 import { useState, useCallback, useEffect } from 'react';
 import { useGame } from '../context/GameContext';
 import { generateSeed, generateSpinResult, generateHash } from '../utils/provablyFair';
